1f9a4434b8d264d822a5a74ebf079022bac9a8c5
c47 <oskar.bing@gmail.com>
2018-05-24 23:10:52 +0300

    Blank project

---
+++ KindIsDead/Source/KindIsDead/KindIsDeadGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KindIsDeadGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINDISDEAD_API AKindIsDeadGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
